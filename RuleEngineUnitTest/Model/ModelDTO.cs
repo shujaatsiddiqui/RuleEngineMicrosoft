aabc99ddbd282d11e3a22d651c2c58e33c52d393
shujaatsiddiqui <shujaatsiddiqui91@gmail.com>
2021-07-06 14:47:38 +0500

    Added Moderte Cases Support

--- RuleEngineUnitTest/Model/ModelDTO.cs
+++ RuleEngineUnitTest/Model/ModelDTO.cs
@@ -15,22 +15,34 @@ namespace RuleEngineUnitTest.Model
         public int SCCR = 30;
         public int FLA = 600;
         public int QTY = 0;
+        public int Hz = 0;
         public string StationType = "Vertical Turbine";
+
+        public int VTPumpsInOrder { get; set; } = 0;
+        public int VTPumpsInGrid { get; set; } = 0;
+        public int VTColumnsInOrder { get; set; } = 0;
+        public List<OptionsList> OptionsLists { get; set; }
+
+        public List<PumpConfiguration> PumpConfigurations { get; set; }
     }
 
     /// <summary>
     /// Workflow Name: Generate Warning - Pump Station
     /// Workflow Name: Auto Check - Pump Station
     /// </summary>
-    public class PumpConfigurations
+    public class PumpConfiguration
     {
-        public PumpConfigurations()
+        public PumpConfiguration()
         {
-            ProductFamily = new ProductFamily();
+            //ProductFamily = new ProductFamily();
         }
         public string PumpDescription { get; internal set; }
         public string VFD { get; internal set; }
-        public ProductFamily ProductFamily { get; set; }
+
+        public int HP { get; internal set; }
+
+        public bool AutoPick { get; set; } = false;
+        //public ProductFamily ProductFamily { get; set; }
 
     }
 
@@ -43,26 +55,28 @@ namespace RuleEngineUnitTest.Model
     {
         public OptionsList()
         {
-            ProductFamily = new ProductFamily();
+            //ProductFamily = new ProductFamily();
         }
         public string OptionNumber { get; set; }
         public string Category { get; set; }
-        public ProductFamily ProductFamily { get; set; }
+
+        public int QTY { get; set; }
+        //public ProductFamily ProductFamily { get; set; }
 
     }
 
-    public class OptionsList2
-    {
-        public OptionsList2()
-        {
-            ProductFamily = new ProductFamily();
-        }
-        public List<PumpConfigurations> PumpConfigurations { get; set; }
-        public string OptionNumber { get; set; }
-        public string Category { get; set; }
-        public ProductFamily ProductFamily { get; set; }
+    //public class OptionsList2
+    //{
+    //    public OptionsList2()
+    //    {
+    //        ProductFamily = new ProductFamily();
+    //    }
+    //    public List<PumpConfigurations> PumpConfigurations { get; set; }
+    //    public string OptionNumber { get; set; }
+    //    public string Category { get; set; }
+    //    public ProductFamily ProductFamily { get; set; }
 
-    }
+    //}
 
 
 
--- RuleEngineUnitTest/RuleEngineTest.cs
+++ RuleEngineUnitTest/RuleEngineTest.cs
@@ -13,112 +13,203 @@ namespace RuleEngineUnitTest
 
     public class RuleEngineTest
     {
+        //[Fact]
+        //public async void SCCRCheck()
+        //{
+        //    var input = new ProductFamily();
+
+        //    RulesEngine.RulesEngine re = GetRuleEngineObj();
+        //    var ruleParameter = new RuleParameter("PumpStationConfiguration", input);
+
+        //    var resultList = await re.ExecuteAllRulesAsync("SCCRCheck", ruleParameter);
+
+        //    //input.PumpConfiguration.ForEach(x=>x.QTY = 5);
+
+        //    Assert.True(resultList.All(r => r.IsSuccess == false));
+
+        //}
+
+        //[Fact]
+        //public async void CheckforNonRegulatingPumpsonVFDs()
+        //{
+        //    var input = new List<PumpConfigurations>();
+
+        //    input = new List<PumpConfigurations>()
+        //    { new PumpConfigurations() { PumpDescription = "Water Feature", VFD = "None" },
+        //          new PumpConfigurations() { PumpDescription = "Transfer", VFD = "None" }
+        //    };
+
+        //    RulesEngine.RulesEngine re = GetRuleEngineObj();
+        //    List<List<RuleResultTree>> lstRuleResultTree = new List<List<RuleResultTree>>();
+        //    foreach (var item in input)
+        //    {
+
+        //        var ruleParameter = new RuleParameter("PumpConfiguration", item);
+
+        //        lstRuleResultTree.Add(await re.ExecuteAllRulesAsync("Check for Non Regulating Pumps on VFDs", ruleParameter));
+        //    }
+        //    Assert.Contains(lstRuleResultTree, r => r.Any(r => r.IsSuccess));
+
+        //}
+
+        //[Fact]
+        //public async void GenerateWarningReport()
+        //{
+        //    var input = new ProductFamily();
+
+        //    RulesEngine.RulesEngine re = GetRuleEngineObj("\\RuleWithSingleWorkflow.json");
+        //    var ruleParameter = new RuleParameter("PumpStationConfiguration", input);
+        //    var resultList = await re.ExecuteAllRulesAsync("GenerateWarningReport", ruleParameter);
+
+        //    foreach (var ruleResult in resultList)
+        //    {
+        //        if (ruleResult.ActionResult != null)
+        //        {
+        //            Console.WriteLine(ruleResult.ActionResult.Output); //ActionResult.Output contains the evaluated value of the action
+        //        }
+        //    }
+
+        //    resultList.OnSuccess((Context) =>
+        //    {
+        //        var a = $"Discount offered is {Context} % over MRP.";
+        //    });
+
+        //    foreach (var result in resultList.Where(r => r.IsSuccess).ToList())
+        //    {
+        //        Console.WriteLine($"{result.Rule.ErrorMessage}");
+        //    }
+
+        //    // all validation rules failed mean no error in configuration.
+        //    Assert.True(resultList[0].IsSuccess == false);
+
+        //}
+
+        //[Fact]
+        //public async void CheckforPumpRatchets190()
+        //{
+        //    var input = new ProductFamily();
+        //    // var a = PumpStationConfiguration.OptionsLists.Where(r => r.OptionNumber.StartsWith("005")).Select(r => r).Sum(r => r.QTY);
+
+        //    //var isTrue = (input.StationType == "Vertical Turbine"
+        //    //                || input.StationType == "Vertical Turbine Quick Ship")
+        //    //                && input.QTY > 0
+        //    //                && input.PumpConfiguration.Select(r=>r.)
+        //    //                && (input.PumpConfiguration.Any(r => r.PumpDescription == "Water Feature" && r.VFD == "None"));
+
+
+        //    RulesEngine.RulesEngine re = GetRuleEngineObj();
+        //    var ruleParameter = new RuleParameter("PumpStationConfiguration", input);
+
+        //    Rule rule = new Rule();
+        //    rule.Operator = "AND";
+        //    rule.Rules = new List<Rule>() { new Rule() { } };
+
+        //    var resultList = await re.ExecuteAllRulesAsync("Check for Pump Ratchets 190", ruleParameter);
+
+
+        //    Assert.True(resultList.All(r => r.IsSuccess));
+
+        //}
+
+        #region Moderate Cases
         [Fact]
-        public async void SCCRCheck()
+        public async void CheckforAgencyType005()
         {
             var input = new ProductFamily();
-
-            RulesEngine.RulesEngine re = GetRuleEngineObj();
-            var ruleParameter = new RuleParameter("PumpStationConfiguration", input);
-
-            var resultList = await re.ExecuteAllRulesAsync("SCCRCheck", ruleParameter);
-
-            //input.PumpConfiguration.ForEach(x=>x.QTY = 5);
-
-            Assert.True(resultList.All(r => r.IsSuccess == false));
-
-        }
-
-        [Fact]
-        public async void CheckforNonRegulatingPumpsonVFDs()
-        {
-            var input = new List<PumpConfigurations>();
-
-            input = new List<PumpConfigurations>()
-            { new PumpConfigurations() { PumpDescription = "Water Feature", VFD = "None" },
-                  new PumpConfigurations() { PumpDescription = "Transfer", VFD = "None" }
+            input.OptionsLists = new List<OptionsList>()
+            {
+                new OptionsList(){ OptionNumber = "005", QTY = 0 },
+                 new OptionsList(){ OptionNumber = "005", QTY = 0 }
             };
 
-            RulesEngine.RulesEngine re = GetRuleEngineObj();
-            List<List<RuleResultTree>> lstRuleResultTree = new List<List<RuleResultTree>>();
-            foreach (var item in input)
-            {
+            RulesEngine.RulesEngine re = GetRuleEngineObj("ModerateScenarios.json");
+            var ruleParameter = new RuleParameter[] { new RuleParameter("ProductFamily", input) };
 
-                var ruleParameter = new RuleParameter("PumpConfiguration", item);
+            var resultList = await re.ExecuteActionWorkflowAsync("ModerateCases", "CheckforAgencyType005", ruleParameter);
 
-                lstRuleResultTree.Add(await re.ExecuteAllRulesAsync("Check for Non Regulating Pumps on VFDs", ruleParameter));
-            }
-            Assert.Contains(lstRuleResultTree, r => r.Any(r => r.IsSuccess));
+            Assert.True(resultList.Results.All(r => r.IsSuccess));
 
         }
 
         [Fact]
-        public async void GenerateWarningReport()
+        public async void CheckforVTDischargeHeadErrors140()
         {
             var input = new ProductFamily();
+            input.VTPumpsInOrder = 1;
 
-            RulesEngine.RulesEngine re = GetRuleEngineObj("\\RuleWithSingleWorkflow.json");
-            var ruleParameter = new RuleParameter("PumpStationConfiguration", input);
-            var resultList = await re.ExecuteAllRulesAsync("GenerateWarningReport", ruleParameter);
-
-            foreach (var ruleResult in resultList)
+            input.OptionsLists = new List<OptionsList>()
             {
-                if (ruleResult.ActionResult != null)
-                {
-                    Console.WriteLine(ruleResult.ActionResult.Output); //ActionResult.Output contains the evaluated value of the action
-                }
-            }
+                new OptionsList(){ OptionNumber = "140", QTY = 0 },
+                 new OptionsList(){ OptionNumber = "140", QTY = 0 }
+            };
 
-            resultList.OnSuccess((Context) =>
-            {
-                var a = $"Discount offered is {Context} % over MRP.";
-            });
+            RulesEngine.RulesEngine re = GetRuleEngineObj("ModerateScenarios.json");
+            var ruleParameter = new RuleParameter[] { new RuleParameter("ProductFamily", input) };
 
-            foreach (var result in resultList.Where(r => r.IsSuccess).ToList())
-            {
-                Console.WriteLine($"{result.Rule.ErrorMessage}");
-            }
+            var resultList = await re.ExecuteActionWorkflowAsync("ModerateCases", "CheckforVTDischargeHeadErrors140", ruleParameter);
 
-            // all validation rules failed mean no error in configuration.
-            Assert.True(resultList[0].IsSuccess == false);
+            Assert.True(resultList.Results.All(r => r.IsSuccess));
 
         }
+        #endregion
 
+        #region Complex
         [Fact]
-        public async void CheckforPumpRatchets190()
+        public async void CheckforVTPumpSelectionErrors12101()
         {
-            var input = new ProductFamily();
+            var ProductFamily = new ProductFamily() { Hz = 50 };
 
-            //var isTrue = (input.StationType == "Vertical Turbine"
-            //                || input.StationType == "Vertical Turbine Quick Ship")
-            //                && input.QTY > 0
-            //                && input.PumpConfiguration.Select(r=>r.)
-            //                && (input.PumpConfiguration.Any(r => r.PumpDescription == "Water Feature" && r.VFD == "None"));
+            ProductFamily.OptionsLists = new List<OptionsList>()
+            {
+                new OptionsList(){ Category = "150", OptionNumber = "00032", QTY = 1 },
+                 new OptionsList(){ Category = "150", OptionNumber = "00045", QTY = 1 }
+            };
 
+            ProductFamily.PumpConfigurations = new List<PumpConfiguration>()
+            {
+                new PumpConfiguration(){HP = 5,PumpDescription = "Transfer" },
+                new PumpConfiguration(){HP = 60,PumpDescription = "Water Feature" }
+            };
 
-            RulesEngine.RulesEngine re = GetRuleEngineObj();
-            var ruleParameter = new RuleParameter("PumpStationConfiguration", input);
+            ProductFamily.VTPumpsInGrid = ProductFamily.VTPumpsInOrder = 2;
 
-            Rule rule = new Rule();
-            rule.Operator = "AND";
-            rule.Rules = new List<Rule>() { new Rule() { } };
+            //List<int> intlst = input.OptionsLists.Select(r.OptionNumber.Substring(r.OptionNumber.Length - 2, 2)
 
-            var resultList = await re.ExecuteAllRulesAsync("Check for Pump Ratchets 190", ruleParameter);
+            //var a = ProductFamily.OptionsLists.Where(r => r.Category == "150").Where(r => int.Parse(r.OptionNumber.Substring(r.OptionNumber.Length - 2, 2)) > 24).Select(r => r).Sum(r => r.QTY);
+            //var b = ProductFamily.PumpConfigurations.Any(r => r.PumpDescription == "50Hz");
 
+            RulesEngine.RulesEngine re = GetRuleEngineObj("ComplexScenarios.json");
+            var ruleParameter = new RuleParameter[] { new RuleParameter("ProductFamily", ProductFamily) };
+
+            var resultList = await re.ExecuteAllRulesAsync("CheckforVTPumpSelectionErrors121", ruleParameter);
+
+            //foreach (var item in resultList)
+            //{
+            //    if (item.IsSuccess)
+            //    {
+            //        foreach (var a in ((ProductFamily)item.Inputs["ProductFamily"]).PumpConfigurations)
+            //        {
+            //            if(a.HP = 50)
+            ////        }
+            //    }
+            //}
 
             Assert.True(resultList.All(r => r.IsSuccess));
 
         }
+        #endregion
 
+        #region Private Functions
         private RulesEngine.RulesEngine GetRuleEngineObj(string fileName = "\\Rule.json")
         {
             var lstWorkflows = JsonConvert.DeserializeObject<WorkflowRules[]>(File.
                                 ReadAllText(Directory.GetParent(Environment.CurrentDirectory).
-                                Parent.Parent.FullName + fileName));
+                                Parent.Parent.FullName + "\\RuleEngineJson\\" + fileName));
 
             var re = new RulesEngine.RulesEngine(lstWorkflows, null);
             return re;
 
         }
+        #endregion
     }
 }
